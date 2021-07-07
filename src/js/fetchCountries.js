b9fa29c6644a26ba3708bbb397cd6b06d3d50155
Vladislav Kulachko <vladcoool@gmail.com>
2021-07-08 01:04:10 +0300

    финал(бета версия)

---
+++ src/js/fetchCountries.js
@@ -0,0 +1,78 @@
+import createCountryCard from '../templates/country-info-template.hbs';
+import debounce from 'lodash.debounce';
+import { alert, defaults } from '@pnotify/core';
+import '@pnotify/core/dist/Material.css';
+import '@pnotify/core/dist/PNotify.css';
+import 'material-design-icons/iconfont/material-icons.css';
+defaults.styling = 'material';
+defaults.icons = 'material';
+
+const countriesFoundContainer = document.querySelector('.found-countries-list');
+const countryCard = document.querySelector('.country-card');
+const findInput = document.querySelector('.country-search__input');
+findInput.addEventListener('input', debounce(inputHandler, 500));
+countriesFoundContainer.addEventListener('click', countryListClick);
+
+function inputHandler(e) {
+  if (e.target.value) {
+    fetchCountry(e.target.value)
+      .then(countries => {
+        if (countries.length === 1) {
+          parceMarkupCountryCard(countries);
+          countriesFoundContainer.innerHTML = '';
+        }
+        return countries;
+      })
+      .then(countries => {
+        if (countries.length > 1 && countries.length <= 10) {
+          parceMarkupFoundCountries(countries);
+          countryCard.innerHTML = '';
+        }
+        return countries;
+      })
+      .then(countries => {
+        if (countries.length >= 11) {
+          alert({
+            text: 'Слишком большое колличество стран. Сделайте запрос более специфичным.',
+          });
+          countriesFoundContainer.innerHTML = '';
+          countryCard.innerHTML = '';
+        }
+        return countries;
+      })
+      .catch(() => {
+        alert({
+          text: 'Страна не найдена!',
+        });
+        countriesFoundContainer.innerHTML = '';
+        countryCard.innerHTML = '';
+      });
+  }
+}
+
+function countryListClick(e) {
+  if (e.target.classList.contains('found-countries-list__item')) {
+    fetchCountry(e.target.textContent).then(parceMarkupCountryCard);
+  }
+}
+
+function parceMarkupCountryCard(countries) {
+  countryCard.innerHTML = createCountryCard(countries);
+}
+
+function parceMarkupFoundCountries(countries) {
+  countriesFoundContainer.innerHTML = createMarkupFoundCountries(countries);
+}
+
+function createMarkupFoundCountries(countries) {
+  return countries.reduce(
+    (markup, country) => markup + `<li class="found-countries-list__item">${country.name}</li>`,
+    '',
+  );
+}
+
+function fetchCountry(country) {
+  return fetch(`https://restcountries.eu/rest/v2/name/${country}`).then(responce => {
+    return responce.json();
+  });
+}
